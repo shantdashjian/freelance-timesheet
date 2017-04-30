16dad1b1bb1ff2ffc466d2c7018a81303fde53bd
Shaun Dashjian <shaundashjian@yahoo.com>
2017-04-29 21:51:03 -0600

    Added more components

--- ngFreelanceTimesheet/WebContent/app/appModule/app.module.js
+++ ngFreelanceTimesheet/WebContent/app/appModule/app.module.js
@@ -1,1 +1,27 @@
-angular.module('appModule', ['nav'])
+angular.module('appModule', ['nav', 'static', 'timesheet', 'ngRoute'])
+.config(function($routeProvider){
+	  $routeProvider
+	    .when('/', {
+	      template : '<home>Loading home</home>'
+	    })
+	    .when('/new', {
+	      template : '<new>Loading new</new>'
+	    })
+	    .when('/history', {
+	      template : '<history>Loading history</history>'
+	    })
+	    .when('/report', {
+	      template : '<report>Loading report</report>'
+
+	    })
+	    .when('/about', {
+	      template : '<about>Loading about</about>'
+
+	    })
+	    .when('/history/:id', {
+	      template : '<history-show>Loading history show</history-show>'
+	    })
+	    .otherwise({
+	      template : '<not-found>Loading not found</not-found>'
+	    })
+	});
