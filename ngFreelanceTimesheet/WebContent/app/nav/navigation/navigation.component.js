f097bb298815388b66677e80c639097ecaab65c4
Shaun Dashjian <shaundashjian@yahoo.com>
2017-04-29 20:06:34 -0600

    Added ng file structure and navbar component

---
+++ ngFreelanceTimesheet/WebContent/app/nav/navigation/navigation.component.js
@@ -0,0 +1,8 @@
+angular.module('nav')
+.component('navigation',{
+	templateUrl: 'app/nav/navigation/navigation.component.html',
+	controller: function(){
+		var vm = this;
+	},
+	controllerAs: 'vm'
+})
