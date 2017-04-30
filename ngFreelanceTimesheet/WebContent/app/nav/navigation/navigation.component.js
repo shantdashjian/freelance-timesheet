2b028ffa3e5c3dac634ef28020d7582ec64a9aa6
Shaun Dashjian <shaundashjian@yahoo.com>
2017-04-29 22:56:20 -0600

    Added navigation

--- ngFreelanceTimesheet/WebContent/app/nav/navigation/navigation.component.js
+++ ngFreelanceTimesheet/WebContent/app/nav/navigation/navigation.component.js
@@ -3,6 +3,22 @@ angular.module('nav')
 	templateUrl: 'app/nav/navigation/navigation.component.html',
 	controller: function(){
 		var vm = this;
+		
+		vm.makeActive = function($event){
+			var $allNavbarButtons = angular.element($event.currentTarget).parent().children();
+			$allNavbarButtons.each(function(){
+				$(this).removeClass('active');
+			});
+			angular.element($event.currentTarget).addClass('active');
+		}
+		
+		vm.clearAll = function($event){
+			var $allNavbarButtons = angular.element($event.currentTarget).parent().next().children().first().children();
+			$allNavbarButtons.each(function(){
+				$(this).removeClass('active');
+			});
+//			angular.element($event.currentTarget).addClass('active');
+		}
 	},
 	controllerAs: 'vm'
 })
