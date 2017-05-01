f2e86b3b22552c534a017116eca536f0cd3fea0f
Shaun Dashjian <shaundashjian@yahoo.com>
2017-04-30 19:06:08 -0600

    Added history

--- ngFreelanceTimesheet/WebContent/app/timesheet/history/history.component.js
+++ ngFreelanceTimesheet/WebContent/app/timesheet/history/history.component.js
@@ -1,8 +1,22 @@
 angular.module('timesheet')
 .component('history',{
 	templateUrl: 'app/timesheet/history/history.component.html',
-	controller: function(){
+	controller: function(timesheetService){
 		var vm = this;
+		vm.workItems = [];
+				
+		vm.reload = function() {
+			timesheetService.index()
+			.then(function(response) {
+				vm.workItems = response.data;
+			});			
+		}
+		
+		vm.reload();
+		
+		
 	},
-	controllerAs: 'vm'
+	controllerAs: 'vm',
+	
+
 })
--- ngFreelanceTimesheet/WebContent/app/timesheet/timesheet.service.js
+++ ngFreelanceTimesheet/WebContent/app/timesheet/timesheet.service.js
@@ -52,22 +52,6 @@ angular.module('timesheet')
 			url : BASE_URL + id,
 		})
 	};
-	
-//	service.getTotalPay = function(){
-//		var total = 0;
-//		service.index()
-//		.then(function(response){
-//			var data = response.data;
-//			data.forEach(function(workItem, index, array) {
-//				var itemTotal = workItem.period * workItem.rate;
-//				total += itemTotal;
-//				
-//			});
-//			console.log(total);
-//			return total;
-//		})
-//		
-//	}
-	
+		
 	return service;
 })
