6347ed6f4e91ce209dc60f7a7210bcff138f2e14
Shaun Dashjian <shaundashjian@yahoo.com>
2017-04-30 22:33:16 -0600

    Cleaned up navbar

--- ngFreelanceTimesheet/WebContent/app/appModule/app.module.js
+++ ngFreelanceTimesheet/WebContent/app/appModule/app.module.js
@@ -2,7 +2,7 @@ angular.module('appModule', ['nav', 'static', 'timesheet', 'ngRoute'])
 .config(function($routeProvider){
 	  $routeProvider
 	    .when('/', {
-	      template : '<home>Loading home</home>'
+	      template : '<new>Loading home</new>'
 	    })	    
 	    .when('/history', {
 	      template : '<history>Loading history</history>'
@@ -12,10 +12,6 @@ angular.module('appModule', ['nav', 'static', 'timesheet', 'ngRoute'])
 	    })
 	    .when('/report', {
 	      template : '<report>Loading report</report>'
-
-	    })
-	    .when('/about', {
-	      template : '<about>Loading about</about>'
 
 	    })
 	    .when('/history/:id', {
